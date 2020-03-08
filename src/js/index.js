b626b9c8a464953691b52997fcebedf7b5c29d33
guitarrockstarco <guitarrockstarco@yahoo.com>
2020-03-08 13:24:53 -0400

    Added display list of recipes and character limit to recipe title

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,37 @@
-import axios from 'axios';
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
-async function getResults(query) {
-  try {
-    const res = await axios(
-      `https://forkify-api.herokuapp.com/api/search?q=${query}`
-    );
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (err) {
-    alert(err);
+/**  Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+
+const state = {};
+
+const controlSearch = async () => {
+  // 1. Get query from view
+  const query = searchView.getInput();
+
+  if (query) {
+    // 2. New search object and add to state
+    state.search = new Search(query);
+
+    // 3. Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+
+    // 4. Search for recipies
+    await state.search.getResults();
+
+    // 5. Render results on UI
+    searchView.renderResults(state.search.result);
   }
-}
-getResults('pizza');
+};
+
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
