9dda29b91e06b50c739936ca06d1f5696aa2a13b
guitarrockstarco <guitarrockstarco@yahoo.com>
2020-03-10 21:23:45 -0400

    Added the like and dislike event handler

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import * as listView from './views/listView';
@@ -126,6 +127,35 @@ elements.shopping.addEventListener('click', e => {
   }
 });
 
+// LIKES CONTROLLER
+const controlLike = () => {
+  if (!state.likes) state.likes = new Likes();
+  const currentID = state.recipe.id;
+
+  // User has NOT yet likes current recipe
+  if (!state.likes.isLiked(currentID)) {
+    // Add like to the state
+    const newLike = state.likes.addLike(
+      currentID,
+      state.recipe.title,
+      state.recipe.author,
+      state.recipe.img
+    );
+    // Toggle the like button
+    // Add like to UI list
+
+    console.log(state.likes);
+    // User HAS liked current recipe
+  } else {
+    // Remove like to the state
+    state.likes.deleteLike(currentID);
+
+    // Toggle the like button
+    // Remove like to UI list
+    console.log(state.likes);
+  }
+};
+
 //  Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
   if (e.target.matches('.btn-decrease, .btn-decrease *')) {
@@ -140,6 +170,9 @@ elements.recipe.addEventListener('click', e => {
     recipeView.updateServingsIngredients(state.recipe);
   } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
     controlList();
+  } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+    // Like controller
+    controlLike();
   }
 });
 
