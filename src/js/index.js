b7883dd737abcaa256bad170953703ef0b06c1e1
guitarrockstarco <guitarrockstarco@yahoo.com>
2020-03-11 19:47:29 -0400

    Added the likes to the UI likes menu

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import * as listView from './views/listView';
+import * as likesView from './views/likesView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /*  Global state of the app
@@ -88,7 +89,7 @@ const controlRecipe = async () => {
 
       // Render recipe
       clearLoader();
-      recipeView.renderRecipe(state.recipe);
+      recipeView.renderRecipe(state.recipe, state.likes.isLiked(id));
     } catch (err) {
       alert('Error');
     }
@@ -128,6 +129,9 @@ elements.shopping.addEventListener('click', e => {
 });
 
 // LIKES CONTROLLER
+//TESTING
+state.likes = new Likes();
+likesView.toggleLikeMenu(state.likes.getNumLikes());
 const controlLike = () => {
   if (!state.likes) state.likes = new Likes();
   const currentID = state.recipe.id;
@@ -142,7 +146,10 @@ const controlLike = () => {
       state.recipe.img
     );
     // Toggle the like button
+    likesView.toggleLikeBtn(true);
+
     // Add like to UI list
+    likesView.renderLike(newLike);
 
     console.log(state.likes);
     // User HAS liked current recipe
@@ -151,9 +158,12 @@ const controlLike = () => {
     state.likes.deleteLike(currentID);
 
     // Toggle the like button
+    likesView.toggleLikeBtn(false);
+
     // Remove like to UI list
-    console.log(state.likes);
+    likesView.deleteLike(currentID);
   }
+  likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
 
 //  Handling recipe button clicks
